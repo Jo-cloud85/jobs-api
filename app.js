64ebd7f5d1538dc05da854f0e24c916fe5dfea3e
JoTYF <joan_tta@live.com.sg>
2023-08-29 00:11:25 +0800

    Minor change

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ app.use(helmet())
 app.use(cors())
 
 app.get('/', (req,res) => {
-    res.send('<h1>jobs API</h1><a href="/api-docs">Documentation</a>')
+    res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>')
 })
 
 app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
