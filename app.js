b4454cb795e1ea3a01c0747fc5629b25fb0f6d4d
JoTYF <joan_tta@live.com.sg>
2023-09-24 14:13:56 +0800

    install xss-clean

--- app.js
+++ app.js
@@ -1,70 +1,72 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import 'express-async-errors';
+import express from "express";
+import dotenv from "dotenv";
+import "express-async-errors";
 
 // extra security packages
-import helmet from 'helmet'
-import cors from 'cors'
-// import { JSDOM } from 'jsdom';
-// import DOMPurify from 'dompurify';
-import rateLimiter from 'express-rate-limit'
+import helmet from "helmet";
+import cors from "cors";
+import xss from "xss-clean";
+import rateLimiter from "express-rate-limit";
 
 // swagger ui
-import swaggerUI from 'swagger-ui-express'
-import YAML from 'yamljs'
-const swaggerDocument = YAML.load('./swagger.yaml')
+import swaggerUI from "swagger-ui-express";
+import YAML from "yamljs";
+const swaggerDocument = YAML.load("./swagger.yaml");
 
 // connectDB
-import connectDB from './db/connect.js'
-import authenticateUser from './middleware/authentication.js'
+import connectDB from "./db/connect.js";
+import authenticateUser from "./middleware/authentication.js";
 
 // routers
-import authRouter from './routes/routes_auth.js'
-import jobsRouter from './routes/routes_jobs.js'
+import authRouter from "./routes/routes_auth.js";
+import jobsRouter from "./routes/routes_jobs.js";
 
 // middleware
-import notFoundMiddleware from './middleware/not-found.js';
-import errorHandlerMiddleware from './middleware/error-handler.js';
+import notFoundMiddleware from "./middleware/not-found.js";
+import errorHandlerMiddleware from "./middleware/error-handler.js";
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-dotenv.config()
+dotenv.config();
 
 // security packages
-app.set('trust proxy', 1)
-app.use(rateLimiter({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-	max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
-	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
-}))
+app.set("trust proxy", 1);
+app.use(
+	rateLimiter({
+		windowMs: 15 * 60 * 1000, // 15 minutes
+		max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+		standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+		legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+	})
+);
 app.use(express.json());
-app.use(helmet())
-app.use(cors())
+app.use(helmet());
+app.use(xss());
+app.use(cors());
 
-app.get('/', (req,res) => {
-    res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>')
-})
+app.get("/", (req, res) => {
+	res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>');
+});
 
-app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 // routes
-app.use('/api/v1/auth', authRouter)
-app.use('/api/v1/jobs', authenticateUser, jobsRouter)
-
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/jobs", authenticateUser, jobsRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
-
 const start = async () => {
-    try {
-        await connectDB(process.env.MONGO_URL)
-        app.listen(port, () => console.log(`Server is listening on port ${port}...`));
-    } catch (error) {
-        console.log(error);
-    }
+	try {
+		await connectDB(process.env.MONGO_URL);
+		app.listen(port, () =>
+			console.log(`Server is listening on port ${port}...`)
+		);
+	} catch (error) {
+		console.log(error);
+	}
 };
 
 start();
